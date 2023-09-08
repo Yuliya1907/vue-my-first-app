d7d004f91d6cb33091c9591735d60d0ab5675f23
Yuliya1907 <violet1907@ukr.net>
2023-09-08 14:38:36 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+      <li>Здоров'я</li>
+      <li>Дім і побут</li>
+      <li>Харчування</li>
+      
+    </ul>
+</template>
+
+<style scoped>
+ul {
+    display:flex;
+    flex-direction: row;
+}
+
+li {
+    font-size: 24px;
+    color: black;
+}
+</style>
